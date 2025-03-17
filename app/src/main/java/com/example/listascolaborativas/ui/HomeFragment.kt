e66937c5b4c51e30c14ad1c888582262f657c820
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2025-03-17 17:55:02 -0300

    feat: adiciona função para listar as listas no HomeFragment

--- app/src/main/java/com/example/listascolaborativas/ui/HomeFragment.kt
+++ app/src/main/java/com/example/listascolaborativas/ui/HomeFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listascolaborativas.R
+import com.example.listascolaborativas.adapter.ListaAdapter
 import com.example.listascolaborativas.databinding.FragmentHomeBinding
+import com.example.listascolaborativas.helper.FirebaseHelper
+import com.example.listascolaborativas.model.Lista
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var listaAdapter: ListaAdapter
+
+    private val listaLista = mutableListOf<Lista>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +47,47 @@ class HomeFragment : Fragment() {
         binding.btnFlutuanteAdd.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_addListaFragment)
         }
+
+        getLista()
     }
+
+    private fun getLista() {
+        FirebaseHelper
+            .getDatabase()
+            .child("lista")
+            .child(FirebaseHelper.getUserId() ?: "")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+
+                        listaLista.clear()
+
+                        for (snap in snapshot.children) {
+                            val lista = snap.getValue(Lista::class.java) as Lista
+                            listaLista.add(lista)
+                        }
+
+                        initAdapter()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Erro ao tentar recuperar as tabelas.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    private fun initAdapter() {
+        binding.recyclerViewLista.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewLista.setHasFixedSize(true)
+        listaAdapter = ListaAdapter(listaLista)
+        binding.recyclerViewLista.adapter = listaAdapter
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
