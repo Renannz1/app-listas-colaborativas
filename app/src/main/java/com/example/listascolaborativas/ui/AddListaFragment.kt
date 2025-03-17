3bb8c83faf19d1f2f206d84b7faa6c86a38d613a
Renan Magalhães <124892507+Renannz1@users.noreply.github.com>
2025-03-17 14:22:55 -0300

    feat: adiona função de criar lista em AddListaFragment

--- app/src/main/java/com/example/listascolaborativas/ui/AddListaFragment.kt
+++ app/src/main/java/com/example/listascolaborativas/ui/AddListaFragment.kt
@@ -5,56 +5,71 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.listascolaborativas.R
+import com.example.listascolaborativas.databinding.FragmentAddListaBinding
+import com.example.listascolaborativas.helper.FirebaseHelper
+import com.example.listascolaborativas.model.Lista
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddListaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddListaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentAddListaBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var lista: Lista
+    private  var newLista: Boolean = true
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =  FragmentAddListaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSave.setOnClickListener {
+            val titulo = binding.edtTituloLista.text.toString()
+
+            if(titulo.isNotEmpty()) {
+                if (newLista) lista = Lista()
+                lista.titulo = titulo
+
+                saveLista()
+            } else {
+                Toast.makeText(requireContext(), "Informe o título da sua lista.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_lista, container, false)
+    private fun saveLista() {
+        FirebaseHelper
+            .getDatabase().child("lista")
+            .child(FirebaseHelper.getUserId() ?: "")
+            .child(lista.id)
+            .setValue(lista)
+            .addOnCompleteListener{ lista ->
+                if (lista.isSuccessful){
+                    if (newLista){ // Criando nova tarefa
+                        findNavController().popBackStack()
+                        Toast.makeText(requireContext(), "Lista criada.", Toast.LENGTH_SHORT).show()
+                    } else { // Editando tarefa
+                        Toast.makeText(requireContext(), "Lista atualizada.", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Erro ao tentar criar a lista.", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), "Erro ao tentar criar a lista.", Toast.LENGTH_SHORT).show()
+            }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddListaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddListaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
